fd183e3961883cbe5bd31a44b3f3492d6b75e771
ericr <36229639+ericr6@users.noreply.github.com>
2019-03-05 16:24:06 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,13 +4,10 @@ arg1=$1
 #prefix for images 
 arg2=$2
 #Data input for nilearn.zip
-NIDIR=/iexec/nilearn_data
+NIDIR=/iexec_in/nilearn_data
 DATAPATH_NILEARN=~/. 
 
-echo "ls ${NIDIR}"
-ls $NIDIR
-
-# wget https://transfer.sh/wTlV3/input_nilearn.zip
+# Data Management 
 if [ -d $NIDIR ]; then
   echo "data input exists"  
   mv $NIDIR $DATAPATH_NILEARN
@@ -19,8 +16,4 @@ else
   echo "data not found ... download"  
 fi
 
-# args 1 is the directory where to save image , args 2 is the prefix for images.
-# if args are missing, program failed.
-
-echo "python3 plot_3d_and_4d_niimg_nogui.py ${arg1} ${arg2}"
 python3 plot_3d_and_4d_niimg_nogui.py $arg1 $arg2
